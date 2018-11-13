8ff8c95acacac778f530373709975f52bbea650b
Isaac Martinez DAM <cf17isaac.martinez@iesjoandaustria.org>
2018-11-13 20:03:53 +0100

    Exercice Strings2 EX3 DONE

--- UF1/Bateria2_Streams/Streams_2/EX3_EscribirFicherotexto.java
+++ UF1/Bateria2_Streams/Streams_2/EX3_EscribirFicherotexto.java
@@ -1,5 +1,19 @@
 package Bateria2_Streams.Streams_2;
 
-public class EX3_EscribirFicherotexto {
+import java.io.*;
 
+public class EX3_EscribirFicherotexto {
+	public static void main (String [] args) throws IOException {
+		File fichero = new File("FicheroTexto.txt");
+		FileWriter fic = new FileWriter (fichero);
+		String prov[] = {"Albacete", "Avila", "Badajoz", "Caceres", "Huelva", "Jaen", "Madrid",
+				"Segovia", "Soria", "Toledo", "Valladolid", "Zamora"};    
+
+		for (int i = 0; i < prov.length-1; i++) {
+			fic.write(prov[i] + ", "); // Escribimos ciudad a ciudad
+		}
+
+		fic.append(prov[prov.length-1]); //Añadimos la ultima ciudad (sin coma al final)
+		fic.close ();   // cerramos fichero
+	}
 }
