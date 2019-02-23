ca7d6ba74a7af3e23af75a96c069db3c4e2cd3c0
Isaac DAM <cf17isaac.martinez@iesjoandaustria.org>
2019-02-23 18:23:15 +0100

    First steps in NeoDartis proyect

---
+++ UF2/proyecto_NeoDartis/Depart.java
@@ -0,0 +1,47 @@
+package proyecto_NeoDartis;
+
+class Depart {
+	private int deptNo;
+	private String dnombre;
+	private String loc;
+	
+	public Depart() {
+		super();
+	}
+	
+	public Depart(int deptNo, String dnombre, String loc) {
+		super();
+		this.deptNo = deptNo;
+		this.dnombre = dnombre;
+		this.loc = loc;
+	}
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
+	public String getDnombre() {
+		return dnombre;
+	}
+
+	public void setDnombre(String dnombre) {
+		this.dnombre = dnombre;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+	@Override
+	public String toString() {
+		return dnombre;
+	}
+}
